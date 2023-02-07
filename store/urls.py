ec5481e84897ce04f646fec55817e5a13e7fb905
Niramay Thaker <thakerniramay@gmail.com>
2023-02-07 18:22:23 +0530

    Templates setup

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = {
 	path("", views.store, name='store'),
+	path("store", views.store, name='store'),
 	path("cart/", views.cart, name='cart'),
 	path("checkout/", views.checkout, name='checkout'),
 }
