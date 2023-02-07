9d847fc3f8c553d1bc4b4119599896d58b4a7638
Niramay Thaker <thakerniramay@gmail.com>
2023-02-07 18:25:08 +0530

    Urls setup

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
 	path("", views.store, name='store'),
 	path("store", views.store, name='store'),
 	path("cart/", views.cart, name='cart'),
 	path("checkout/", views.checkout, name='checkout'),
-}
+]
