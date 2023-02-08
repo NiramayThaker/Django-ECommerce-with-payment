3f87e32dac5c91e07c74f033adcc50efd8db1841
Niramay Thaker <thakerniramay@gmail.com>
2023-02-08 17:34:29 +0530

    Model Creation

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Customer(models.Model):
+	# One-to-One relationship means that a user can only have one customer and customer can only have one user
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200)
+
+	def _str__(self):
+		return self.name
