1963a1d960f61ab5dc4943b49be97b706697c907
Niramay Thaker <thakerniramay@gmail.com>
2023-02-09 17:37:04 +0530

    Handling media exception

--- store/models.py
+++ store/models.py
@@ -18,10 +18,20 @@ class Product(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False, null=True, blank=False)
-	image = models.ImageField(null=True, blank=True)
+	image = models.ImageField(null=True, blank=True, upload_to='static/images/',
+							  default='static/images/placeholder.jpg')
 
 	def __str__(self):
 		return self.name
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ""
+
+		return url
 
 
 class Order(models.Model):
