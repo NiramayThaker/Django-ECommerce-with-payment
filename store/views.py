78fde59ffde3a7a76dd9fa12e9eff114e6b8588c
Niramay Thaker <thakerniramay@gmail.com>
2023-02-13 19:26:33 +0530

    Add to cart functionality

--- store/views.py
+++ store/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+import json
 
 
 # Create your views here.
 def store(request):
 	products = Product.objects.all()
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		# It will get all the orderItems which will have "order" as parent
+		items = order.orderitem_set.all()
+		cart_items = order.get_cart_items
+	else:
+		items = []
+		order = {'get_cart_total': 0,
+				 'get_cart_items': 0
+				 }
+		cart_items = order['get_cart_items']
 
-	context = {'products': products}
+	context = {'products': products, 'cart_items': cart_items, 'order': order}
 	return render(request, 'store/store.html', context=context)
 
 
@@ -42,4 +55,24 @@ def checkout(request):
 
 
 def updateItem(request):
+	data = json.loads(request.body)
+	product_id = data['productId']
+	action = data['action']
+	print("product:", product_id)
+	print("action:", action)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=product_id)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		order_item.quantity += 1
+	elif action == 'remove':
+		order_item.quantity -= 1
+	order_item.save()
+
+	if order_item.quantity <= 0:
+		order_item.delete()
+
 	return JsonResponse("Item added successfully", safe=False)
